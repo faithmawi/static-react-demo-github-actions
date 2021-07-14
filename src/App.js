c2290b736d569194f0e5f9c18cc46ba185694cfb
faithmawi <faith.elikirmawi@infinityworks.com>
2021-07-14 14:29:07 +0100

    css image height

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="profile.jpg" alt="logo" />
+        <img id="hi" src="profile.jpg" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
